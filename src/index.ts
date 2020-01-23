84ed4c48d5211ecaad801407386c366b64b35495
margaiwangara <margaiwangara@gmail.com>
2020-01-23 21:32:01 +0300

    added config for prettier linting

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import express, { Request, Response, NextFunction, Application } from "express";
-import * as dotenv from "dotenv";
-import * as path from "path";
+import express, { Request, Response, NextFunction, Application } from 'express';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const x = 500;
 
 // dotenv config
-dotenv.config({ path: path.resolve(__dirname, "../config/config.env") });
+dotenv.config({ path: path.resolve(__dirname, '../config/config.env') });
 // Initialize App
 const app: Application = express();
 
 // Routes
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
 });
 
 // Running app on PORT
 const PORT: number = parseInt(`${process.env.PORT}`, 10) || 5001;
 
 app.listen(PORT, () =>
-  console.log(`App running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`App running in ${process.env.NODE_ENV} mode on port ${PORT}`),
 );

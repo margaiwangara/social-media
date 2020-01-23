3535dcd198aaf864c5a4ed18169088faf581efad
margaiwangara <margaiwangara@gmail.com>
2020-01-24 01:35:51 +0300

    created an error exception handler

---
+++ src/handlers/HttpException.ts
@@ -0,0 +1,11 @@
+class HttpException extends Error {
+  status: number;
+  message: string;
+  constructor(status: number, message: string) {
+    super(message);
+    this.message = message;
+    this.status = status;
+  }
+}
+
+export default HttpException;

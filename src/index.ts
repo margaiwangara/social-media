f74a9d9cdfee2a209c298a52475f380a1d5f1692
margaiwangara <margaiwangara@gmail.com>
2020-01-23 19:10:13 +0300

    added tsconfig file for typescript config, added run scripts, added test code for ts build testing

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response, NextFunction, Application } from "express";
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+// dotenv config
+dotenv.config({ path: path.resolve(__dirname, "../config/config.env") });
+// Initialize App
+const app: Application = express();
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+// Running app on PORT
+const PORT: number = parseInt(`${process.env.PORT}`, 10) || 5001;
+
+app.listen(PORT, () =>
+  console.log(`App running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);

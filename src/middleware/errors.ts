db36a932cd5d543ab28b01713c3d8093a211c74f
margaiwangara <margaiwangara@gmail.com>
2020-01-24 18:52:33 +0300

    completed custom error handling operation

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -5,12 +5,36 @@ function errorMiddleware(
   error: HttpException,
   request: Request,
   response: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) {
-  console.log(error);
-  return response.status(error.status || 500).json({
+  let err: HttpException = { ...error };
+  let message: string;
+  switch (err.name) {
+    case 'CastError':
+      message = 'Resource Not Found';
+      err = new HttpException(404, message);
+      break;
+    case 'ValidationError':
+      message = JSON.stringify(
+        Object.values((err as any).errors).map((value: any) => value.message)
+      );
+      err = new HttpException(400, message);
+      break;
+    default:
+      message = 'Oops! Something went wrong';
+      err = new HttpException(500, message);
+      break;
+  }
+  // Duplicate input error
+  if (err.status === 11000) {
+    message = 'Duplicate Field Value Entered';
+    err = new HttpException(400, message);
+  }
+  console.log(process.env.NODE_ENV === 'development' && error);
+  return response.status(err.status).json({
     error: {
-      message: error.message || 'Oops! Something went wrong'
+      message: err.message
     }
   });
 }

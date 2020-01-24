9300a6651a0032d58f29fc4a40b3748ecb73fe20
margaiwangara <margaiwangara@gmail.com>
2020-01-24 21:33:03 +0300

    added react folder for front-end, changed scripts and added concurrently for client-server same time loading

---
+++ client/README.md
@@ -0,0 +1,1 @@
+## Social Media - Simple social media application
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+     Create React App
+    </div>
+  );
+}
+
+export default App;

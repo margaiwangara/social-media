37a285b3e3f0e94514da084408ae43ab05d08ba3
margaiwangara <margaiwangara@gmail.com>
2020-01-24 22:52:32 +0300

    simple modifications

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Register = () => {
+const Register: React.FC = () => {
   return (
-    <form method="post">
-      <label htmlFor="nameField">Name</label>
-      <input type="text" placeholder="Name" id="nameField"/>
-      <label htmlFor="surnameField">Surname</label>
-      <input type="text" placeholder="Surname" id="surnameField"/>
-      <label htmlFor="emailAddress">Email</label>
-      <input type="email" placeholder="Email" id="emailAddress"/>
-      <label htmlFor="passwordField">Password</label>
-      <input type="password" name="password" id="passwordField"/>
-      <button type="submit">Register</button>
-    </form>
+    <button>Click Me</button>
   )
 }
 

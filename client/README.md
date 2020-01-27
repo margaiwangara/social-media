61b7a09cfc2ccf7422a757fe495d82f47e688dfb
margaiwangara <margaiwangara@gmail.com>
2020-01-27 09:49:46 +0300

    React Client - Initial Commit

---
+++ client/README.md
@@ -0,0 +1,3 @@
+## Social Media React Front-End
+
+Front-end of the social media app written in React.js
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Create React App
+    </div>
+  );
+}
+
+export default App;

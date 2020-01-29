294f006966da15f4ddcf845666c4b98449119d79
margaiwangara <margaiwangara@gmail.com>
2020-01-29 16:53:25 +0300

    made homepage styling prettier

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function Homepage(){
-  return (
-    <h4>This is my homepage</h4>
-  )
+function Homepage() {
+  return <h4>This is my homepage</h4>;
 }
 
 export default Homepage;
